61cfd7ea60244679aa8955fd275e095ae77d317e
Hafeefa <hafeefapc2003@gmail.com>
2024-09-27 07:00:17 +0530

    small chage

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CameraScreen(), // This should be your entry point
+      home: CameraScreen(),
     );
   }
 }
